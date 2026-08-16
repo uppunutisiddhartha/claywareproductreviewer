97ac0bc8a2edd81fae2437d0dba541dae5c0917f
Siddhartha Uppunuti <uppunutisiddhartha@gmail.com>
2026-08-17 00:47:15 +0530

    second commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,19 +8,15 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const token =
-            localStorage.getItem("access_token");
+        const token = localStorage.getItem("access_token");
 
         if (token) {
-            config.headers.Authorization =
-                `Bearer ${token}`;
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
         return config;
     },
-    (error) => {
-        return Promise.reject(error);
-    }
+    (error) => Promise.reject(error)
 );
 
 export default api;
