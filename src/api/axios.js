e04311e1894ba1a4df6013c15fa6e605d128f6ac
Siddhartha Uppunuti <uppunutisiddhartha@gmail.com>
2026-08-17 00:52:03 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_URL ||
-        "http://127.0.0.1:8000/api",
+         "https://claywarebackendapis.onrender.com/api",
 });
 
 api.interceptors.request.use(
